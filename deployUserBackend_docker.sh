3057c846518416876576da90b971ae804dcfa27a
Thilo Schenkelberg <thiloschenkelberg@gmail.com>
2024-10-03 11:05:31 +0200

    wip

--- deployUserBackend_docker.sh
+++ deployUserBackend_docker.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Navigate to the userBackendNodeJS directory
 cd userBackendNodeJS/
 
-# Stop the PM2-managed process
 pm2 stop build/server.js
 
-# Remove existing build directory
 rm -rf build/
 
-# Build the Node.js application
 npm run build
 
-# Start the PM2-managed process
 pm2 start build/server.js
