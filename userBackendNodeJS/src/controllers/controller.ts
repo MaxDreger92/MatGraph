f4174781f65e3d5b49881f9276ea1550d9204eb3
Thilo Schenkelberg <thiloschenkelberg@gmail.com>
2024-10-09 09:57:55 +0200

    Remove logs

--- userBackendNodeJS/src/controllers/controller.ts
+++ userBackendNodeJS/src/controllers/controller.ts
@@ -26,7 +26,6 @@ router.post('/api/users/login', async (req, res) => {
             })
         }
 
-        console.log('email: ', password)
         const user = await UserService.findByMail(email)
         if (!user) {
             return res.status(401).json({
