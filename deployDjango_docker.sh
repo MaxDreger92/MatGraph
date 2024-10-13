d67a50756c9a97572fab8777df2549cc81c4a4b5
Thilo Schenkelberg <thiloschenkelberg@gmail.com>
2024-10-13 12:06:34 +0200

    Fix docker settings + Remove some files

--- deployDjango_docker.sh
+++ deployDjango_docker.sh
@@ -3,5 +3,7 @@
 rm -rf mat2devplatform/static/
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
 
-gunicorn --workers=3 mat2devplatform.wsgi:application --bind 0.0.0.0:8000
+gunicorn --workers=3 mat2devplatform.wsgi:application --bind 0.0.0.0:$DJANGO_PORT
